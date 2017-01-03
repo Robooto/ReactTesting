268855f07501b1d9b0183ed1c43c24b834af71df
Sam Russell <samrussell63@gmail.com>
2017-01-02 22:53:04 -0800

    inital commit

---
+++ src/components/comment_box.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { saveComment } from '../actions';
+
+// managing state just using react
+class CommentBox extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { comment: '' };
+    }
+
+    handleChange(event) {
+        this.setState({ comment: event.target.value });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        this.props.saveComment(this.state.comment);
+        this.setState({ comment: ''});
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit.bind(this)} className="comment-box">
+                <h4>Add a comment</h4>
+                <textarea value={this.state.comment} onChange={this.handleChange.bind(this)}></textarea>
+                <br/>
+                <button action="submit" >Submit Comment</button>
+            </form>
+        )
+    }
+}
+
+
+export default connect(null, { saveComment })(CommentBox);
